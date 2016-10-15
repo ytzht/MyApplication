41cc9af6eff7fff0d71d4074ce2728b535f31b90
ytzht <1176692275@qq.com>
2016-10-15 14:50:32 +0800

    注册不好用，登录退出哦了

--- app/src/main/java/cn/fcrj/mychatdemo/activity/BaseActivity.java
+++ app/src/main/java/cn/fcrj/mychatdemo/activity/BaseActivity.java
@@ -1,13 +1,21 @@
 package cn.fcrj.mychatdemo.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(0);
+
+
+
+    }
+
+    public void SnackMsg(View view, String msg){
+        Snackbar.make(view, msg, Snackbar.LENGTH_SHORT).show();
     }
 }
